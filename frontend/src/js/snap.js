ae9bd445a4b281fe9ee7ebfc86701ecf8bc6a563
alexhalcazar <alexhalcazar369@gmail.com>
2024-09-03 21:53:17 -0700

    Basic implementation to display card images from search results

--- frontend/src/js/snap.js
+++ frontend/src/js/snap.js
@@ -7,8 +7,30 @@ document
         try {
             const response = await fetch(`/database/find?card=${card}`);
             const data = await response.json();
-            console.log('Our data', data);
+            cardResults(card, data);
         } catch (error) {
             console.log(error);
         }
     });
+
+const cardResults = (card, cardData) => {
+    try {
+        if (!card) {
+            for (const data of cardData) {
+                displayImage(data);
+            }
+        } else {
+            displayImage(cardData[0]);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const displayImage = (cardData) => {
+    const div = document.getElementById('collection-list');
+    const imgElement = document.createElement('img');
+    imgElement.src = cardData.imgPath;
+    imgElement.alt = cardData.character;
+    div.appendChild(imgElement);
+};
