6a16b0a68ea1880c241216c4bdaccf6b06f4841c
alexhalcazar <alexhalcazar369@gmail.com>
2024-11-19 10:35:44 -0800

    moved parse JSON middleware to upper most level

--- backend/routes/snapRoutes.js
+++ backend/routes/snapRoutes.js
@@ -2,7 +2,6 @@ import express from 'express';
 import { searchCard } from '../controllers/snapController.js';
 
 const router = express.Router();
-router.use(express.json());
 router.post('/find', searchCard);
 router.get('/find', searchCard);
 
