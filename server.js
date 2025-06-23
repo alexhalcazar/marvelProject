d52a273b807182097b1405f39c0fcd664ad23b86
alexhalcazar <alexhalcazar369@gmail.com>
2025-06-23 12:53:31 -0700

    feat: add express.json middleware to parse JSON request bodies

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '/frontend/public')));
-app.use(express.static(path.join(__dirname, '/frontend/src')));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.sendFile(
@@ -34,6 +34,6 @@ app.get('/marvelSnap', (req, res) => {
 app.use('/api/characters', characterRouter);
 app.use('/database', snapRouter);
 
-app.listen(port, async () => {
+app.listen(port, '0.0.0.0', async () => {
     console.log(`server is listening on port ${port}`);
 });
